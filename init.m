a24273b73aa8f755e259bb1127d8d7d75a956a3d
Levi Rogalla <levi.rogalla@gmail.com>
2025-07-14 21:31:04 -0400

    create working robot model

--- init.m
+++ init.m
@@ -25,3 +25,5 @@ world.gravity = 9.8; %m/s^2
 assignin('base', 'robot', robot);
 assignin('base', 'motor', motor);
 assignin('base', 'susp', susp);
+
+https://ethz.ch/content/dam/ethz/special-interest/mavt/dynamic-systems-n-control/idsc-dam/Lectures/amod/AMOD_2020/20201019-02%20-%20ETHZ%20-%20Modeling.pdf
