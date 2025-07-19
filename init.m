c7181db7604f675da5a250b1c560acc40c9061e1
ydzulynsky <yarema.dzulynsky@gmail.com>
2025-07-19 12:27:01 -0400

    single wheel suspension and xy to z map

--- init.m
+++ init.m
@@ -2,7 +2,7 @@
 % Project-wide Parameters
 
 % Robot Geometry
-robot.mass_robot = 20;       % kg
+roCbot.mass_robot = 20;       % kg
 robot.mass_payload = 10;     % kg
 robot.height = 5;            % m
 robot.diameter = 0.5;        % m
@@ -16,6 +16,10 @@ motor.Kt = 0.1;      % Nm/A
 motor.Ke = 0.1;      % V/(rad/s)
 
 % Suspension parameters (tunable in Task 5)
+% NOTE THAT THIS K AND C FOR EACH WHEEL. IF WE ASSUME THE WHEELS
+% ARE IN THE SAME LOCATION ON THE ROBOT THEN WE TREAT THE 1 WHEEL
+% MODEL AS A 2 WHEEL MODEL BY ASSINGING K AND C THE PARALLEL
+% EQUIVILANT K AND C FROM 2 WHEELS.
 susp.k = 1500;   % N/m
 susp.c = 1500;    % Ns/m
 
